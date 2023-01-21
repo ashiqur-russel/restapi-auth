d32c4d588364faabf1211e24a1d2f0dd82322ade
ashiqurras <ashiqurrussel2@gmail.com>
2023-01-22 00:35:00 +0100

    product post and get api and product table create

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get('/products',function(){
+    return Product::all();
+});
+
+Route::post('/products',function(){
+    return Product::create([
+        'name' => 'Product One',
+        'slug' =>'product 1',
+        'description'=>'This is prpduct one',
+        'price'=>'99.99'
+    ]);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
