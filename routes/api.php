2658d39d4931a06ae957dbc40b7c78effa953ad4
ashiqurras <ashiqurrussel2@gmail.com>
2023-01-22 01:57:00 +0100

    search api

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/products/{id}',[ProductController::class,'show']);
 
 Route::resource('products',ProductController::class);
-
+Route::get('/products/search/{name}',[ProductController::class,'search']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
